b4e8be1a9022a6512412c85d51983dc6d18a82a5
Lars Yencken <lars@yencken.org>
2012-05-04 15:38:33 +1000

    A basic process and queue model for the api server.

--- triage/api.py
+++ triage/api.py
@@ -1,14 +1,17 @@
+import os
+from sys import argv
+import logging
+from subprocess import Queue, Process
+
 import zmq
 import msgpack
-import mongoengine
-import logging
-from sys import argv
 from pyramid.paster import get_appsettings
-from models import Error
+
+N_PROCESSES = 4
+SENTINEL = 'XXX'
 
 #logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
-
 
 # config
 logging.info('Loading configuration')
@@ -22,27 +25,62 @@ socket = context.socket(zmq.SUB)
 socket.bind(ZMQ_URI)
 socket.setsockopt(zmq.SUBSCRIBE, '')
 
-# mongo
-logging.info('Connecting to mongo at: mongodb://' + settings['mongodb.host'] + '/' + settings['mongodb.db_name'])
-mongoengine.connect(settings['mongodb.db_name'], host=settings['mongodb.host'])
+class MongoConsumer(Process):
+    def __init__(self, queue):
+        self.queue = queue
+        self.conn = None
+        super(MongoConsumer, self).__init__()
+
+    def run(self):
+        import mongoengine
+        from models import Error
+        while True:
+            # mongo
+            logging.info('Process %d connecting to mongo at: mongodb://' % os.getpid() + settings['mongodb.host'] + '/' + settings['mongodb.db_name'])
+            mongoengine.connect(settings['mongodb.db_name'], host=settings['mongodb.host'])
+
+            msg = self.queue.get()
+            if msg == SENTINEL:
+                break
+
+            try:
+                if type(msg) == dict:
+                    logging.debug('found object in message')
+                    error = Error.create_from_msg(msg)
+                    error.save()
+                    logging.debug('saved error')
+            except Exception:
+                logging.exception('Failed to process error')
 
 # messagepack
 unpacker = msgpack.Unpacker()
 
+queue = Queue()
+pool = [MongoConsumer(queue) for i in xrange(N_PROCESSES)]
+
+for p in pool:
+    p.start()
+
 # serve!
 logging.info('Serving!')
-while True:
-    try:
-        data = socket.recv()
-        logging.debug('received data')
-        unpacker.feed(data)
-        logging.debug('fed data to unpacker')
-        for msg in unpacker:
-            logging.debug('found message in unpacker')
-            if type(msg) == dict:
-                logging.debug('found object in message')
-                error = Error.create_from_msg(msg)
-                error.save()
-                logging.debug('saved error')
-    except Exception, a:
-        logging.exception('Failed to process error')
+try:
+    while True:
+        try:
+            data = socket.recv()
+            logging.debug('received data')
+            unpacker.feed(data)
+            logging.debug('fed data to unpacker')
+            for msg in unpacker:
+                logging.debug('queuing message from unpacker')
+                queue.put(msg)
+        except Exception:
+            logging.exception('Failed to unpack error')
+
+except KeyboardInterrupt:
+    logging.info('Shutting down...')
+    # wait for the processes to end
+    for p in pool:
+        queue.put(SENTINEL)
+    for p in pool:
+        p.wait()
+
